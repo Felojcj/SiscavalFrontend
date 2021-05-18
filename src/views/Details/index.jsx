500fd4f48bdc0b77cd094a87ba2d79b1f7c849c9
Felojcj <felocorrea18@gmail.com>
2021-05-17 22:54:16 -0500

    Adding Details View

--- src/views/Details/index.jsx
+++ src/views/Details/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -12,12 +12,12 @@ import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '50%',
+    maxWidth: '60%',
     margin: '0 auto',
     backgroundColor: theme.palette.background.paper,
   },
@@ -47,6 +47,29 @@ const StyledDetail = styled.div`
 const Details = () => {
   const classes = useStyles()
   const history = useHistory()
+  const [details, setDetails] = useState([])
+  const [failedFetch, setFailedFetch] = useState(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`http://siscaval.edu.co/api/details/${id}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('loginInfo')).token}`
+      }
+    })  
+    .then(res => res.json())
+    .then(json => {
+      if (json.status === '404') {
+        setFailedFetch(true)
+      } else {
+        setDetails(json)
+      }
+    })
+    .catch(err => {
+      setFailedFetch(true)
+    })
+  }, [id])
 
   return (
     <StyledDetail>
@@ -66,40 +89,64 @@ const Details = () => {
           </Button>
         </Grid>
       </Grid>
-      <List className={classes.root}>
-        <ListItem alignItems="center">
-          <ListItemAvatar>
-            <Avatar className={classes.orange}>
-              {JSON.parse(localStorage.getItem('loginInfo')).name[0]}
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary="Fetch Deatil Name"
-            secondary={
-              <Box
-                display="flex"
-                flexDirection="column"
-              >
-                <Box
-                  display="flex"
-                  flexDirection="row"
-                >
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    Fetch Data Type
-                  </Typography>
-                  {" — Fetch Column Name…"}
-                </Box>
-              </Box>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-      </List>
+      {!failedFetch ? (
+        <>
+          {
+            details.map((detail, index) => (
+              <List className={classes.root} key={index}>
+                <ListItem alignItems="center">
+                  <ListItemAvatar>
+                    <Avatar className={classes.orange}>
+                      {JSON.parse(localStorage.getItem('loginInfo')).name[0]}
+                    </Avatar>
+                  </ListItemAvatar>
+                <ListItemText
+                  primary={detail.column_name.charAt(0).toUpperCase() + detail.column_name.slice(1)}
+                  secondary={
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                    >
+                      <Box
+                        display="flex"
+                        flexDirection="row"
+                        justifyContent="space-between"
+                      >
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                        >
+                          {`${detail.data_type.charAt(0).toUpperCase() + detail.data_type.slice(1)}`}
+                        </Typography>
+                        {` Como Minimo Debe Contener ${detail.min_length} Caracteres y un Maximo de ${detail.max_length}`}
+                      </Box>
+                      <Box
+                        display="flex"
+                        flexDirection="row"
+                        justifyContent="flex-end"
+                      >
+                        <Button>Editar</Button>
+                        <Button>Eliminar</Button>
+                      </Box>
+                    </Box>
+                  }
+                />
+                </ListItem>
+              <Divider variant="inset" component="li" />
+              </List>
+            ))
+          }
+        </>
+      ) :
+        <Typography
+          component="h2"
+          color="textPrimary"
+        >
+          Informacion no encontrada
+        </Typography>
+      }
     </StyledDetail>
   )
 }
